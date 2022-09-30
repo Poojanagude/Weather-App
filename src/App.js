808aa1816a8f17edd77b93019a219184e53fa3e0
Poojanagude <102821867+Poojanagude@users.noreply.github.com>
2022-09-29 21:57:23 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Temp from './components/temp';
+
+const App=()=>{
+  return (
+    <>
+    <div className="App">
+      <Temp/>
+    </div>
+    </>
+  );
+};
+
+export default App;
